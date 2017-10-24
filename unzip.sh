41b11a3dd81b46aae55c1cb5f5da323bf37cd8e9
hsinweiyo <hsinweiyo@gmail.com>
2017-10-24 11:44:51 +0800

    Rename folders, submit to moss seperately.

--- unzip.sh
+++ unzip.sh
@@ -1,4 +1,7 @@
 #unzip hw_*.zip
 mv hw_*.zip Lab4.zip
 ls *.zip|awk -F'.zip' '{print "unzip "$0" -d "$1}' | sh
-perl moss.pl -l verilog Lab4/*/*.v
+perl moss.pl -l verilog Lab4/*/*_1.v
+perl moss.pl -l verilog Lab4/*/*_2.v
+perl moss.pl -l verilog Lab4/*/*_3.v
+
